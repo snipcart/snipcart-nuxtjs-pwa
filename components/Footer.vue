b32a090d67ec6c2e582ce7ee0320d86b6628bbe9
Ludovic Armand <77077373+ludovic-armand@users.noreply.github.com>
2021-09-16 09:56:47 -0400

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer text-center mt-20 mb-10 markdown">
     <div>
       <p>Demo built with <a href="https://nuxtjs.org/">Nuxt.js</a> & <a href="https://snipcart.com/">Snipcart</a></p>
-      <p>More PWA goodness<br> in the <a href="https://snipcart.com/blog/vue-pwa/">Blog Post</a> and <a href="https://github.com/snipcart/snipcart-nuxtjs-pwa">Github Repo</a></p>
+      <p>More PWA goodness<br> in the <a href="https://snipcart.com/blog/vue-pwa-development">Blog Post</a> and <a href="https://github.com/snipcart/snipcart-nuxtjs-pwa">Github Repo</a></p>
     </div>
   </footer>
 </template>
